1d6d41c2443eadda1878fec757947007b35d46ed
ShebalkovD <shebalkovdenis.wk@gmail.com>
2025-03-14 16:10:07 +0300

    Проверка клавиши при нажатии

--- src/components/Type.jsx
+++ src/components/Type.jsx
@@ -5,16 +5,25 @@ export default function Type() {
     const textArray = INITIAL_TEXT.split('')
 
     const [counter, setCounter] = useState(0)
+    const [shift, setShift] = useState(0)
+    let currentSymbol = textArray[counter]
     const handleKeyDown = (event) => {
-        console.log('нажатие');
+        if (event.key == currentSymbol) {
+            console.log('верно')
+            setCounter(prev => prev + 1)
+            currentSymbol = textArray[counter]
+        }else {
+            console.log('неверно')
+        }
     }
     
     return(
         <div className="container flex items-center justify-center min-h-[100vh]" onKeyUp={handleKeyDown} tabIndex={0}>
             <p>{counter}</p>
-            <div className="text-line bg-white p-4">
+            <div className="text-line relative bg-white w-[80%] h-12">
                 <div 
-                    className="wrapper"
+                    className={`wrapper absolute top-3`}
+                    style={{left: 10 - shift}}
                 >
                     {textArray.map((symbol, index) => 
                         (<span key={index}>{symbol}</span>)
