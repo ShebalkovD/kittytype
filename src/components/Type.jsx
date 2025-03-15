a8c3721ccf7b036da7cd939d3cbd4f8f86a0390c
ShebalkovD <shebalkovdenis.wk@gmail.com>
2025-03-15 22:08:01 +0300

    Добавлена функция перезапуска тренажера, кнопка рестарт, обработка сочетания клавиш ctrl+enter для рестарта

--- src/components/Type.jsx
+++ src/components/Type.jsx
@@ -24,8 +24,18 @@ export default function Type() {
             setCatImg(catConfig.left)
             catImg == catConfig.left ? setCatImg(catConfig.right) : setCatImg(catConfig.left)
         }else {
-            setCatImg(catConfig.default)
+            if (event.code === 'Enter' && (event.ctrlKey || event.metaKey)) {
+                restart()
+            }else {
+                setCatImg(catConfig.default)
+            }
         }
+    }
+
+    const restart = () => {
+        setCounter(0)
+        setCatImg(catConfig.default)
+        setShift(0)
     }
 
     return(
@@ -50,6 +60,7 @@ export default function Type() {
                     className="block w-12"
                     src={catImg} 
                 />
+
                 <div className="text-line relative w-[100%] h-16 lg:h-12 overflow-hidden rounded-full" style={{backgroundColor: themeConfig.header}}>
                     <div 
                         className={`wrapper absolute top-2 w-fit whitespace-nowrap transition-transform duration-50 ease-linear`}
@@ -67,6 +78,17 @@ export default function Type() {
                             </span>)
                         )}
                     </div>
+                </div>
+
+                <div 
+                    className="opacity-40 hover:opacity-100 hover:cursor-pointer transition-opacity duration-200 ease-in-out mt-30 text-center flex flex-col justify-center items-center hover:[&>p]:opacity-0 hover:[&>p]:translate-y-1"
+                    onClick={restart}
+                >
+                    <svg width="54" height="54" viewBox="0 0 54 54" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M36.364 20.636C31.6777 15.9497 24.0797 15.9497 19.3934 20.636C14.7071 25.3223 14.7071 32.9203 19.3934 37.6066C24.0797 42.2929 31.6777 42.2929 36.364 37.6066" stroke={themeConfig.text} stroke-width="4" stroke-linecap="round"/>
+                        <path d="M37.412 15.6787L30.9082 22.2251C30.2755 22.8621 30.7375 23.9455 31.6352 23.9298L38.2517 23.8138C38.8039 23.8041 39.2437 23.3486 39.234 22.7964L39.1213 16.366C39.1058 15.4821 38.0351 15.0516 37.412 15.6787Z" fill={themeConfig.text}/>
+                    </svg>
+                    <p className="text-[0.8rem] font-bold translate-x-0.5 transition-all duration-200 ease-in-out">Ctrl + Enter</p>
                 </div>
             </div>
             
