1017e02e75e5e8793c94d2c6f9f7607fe0002c3f
ShebalkovD <shebalkovdenis.wk@gmail.com>
2025-03-16 21:52:25 +0300

    Добавлены картинки белого, черного и серого кота. Обновлен конфиг с пресетами котов

--- src/assets/cat.js
+++ src/assets/cat.js
@@ -1,8 +1,23 @@
 let catConfig = {
     orange: {
         default: "/src/assets/images/orangeCat/cat-still.png",
-        "left": "/src/assets/images/orangeCat/cat-left.png",
-        "right": "/src/assets/images/orangeCat/cat-right.png"
+        left: "/src/assets/images/orangeCat/cat-left.png",
+        right: "/src/assets/images/orangeCat/cat-right.png"
+    },
+    white: {
+        default: "/src/assets/images/whiteCat/whitecat-default.png",
+        left: "/src/assets/images/whiteCat/whitecat-left.png",
+        right: "/src/assets/images/whiteCat/whitecat-right.png"
+    },
+    black: {
+        default: "/src/assets/images/blackCat/blackCat-default.png",
+        left: "/src/assets/images/blackCat/blackCat-left.png",
+        right: "/src/assets/images/blackCat/blackCat-right.png"
+    },
+    gray: {
+        default: "/src/assets/images/grayCat/grayCat-default.png",
+        left: "/src/assets/images/grayCat/grayCat-left.png",
+        right: "/src/assets/images/grayCat/grayCat-right.png"
     }
 }
 
