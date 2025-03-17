d13528eccfb123788fe557d60b979f696d128b3f
ShebalkovD <shebalkovdenis.wk@gmail.com>
2025-03-17 11:31:41 +0300

    добавлена иконка таймера, эффект подчеркивания для ошибки

--- src/components/Type.jsx
+++ src/components/Type.jsx
@@ -10,13 +10,15 @@ export default function Type() {
     const textlineRef =  useRef(null)
     const [counter, setCounter] = useState(0)
     const [shift, setShift] = useState(0)
-    const [theme, settheme] = useState(themes.dark)
+    const [theme, setTheme] = useState(themes.dark)
     const [catConfig, setCatConfig] = useState(cats.gray)
     const [catImg, setCatImg] = useState(catConfig.default)
+    const [mistake, setMistake] = useState(false)
     let currentSymbol = textArray[counter]
 
     const handleKeyDown = (event) => {
         if (event.key == currentSymbol) {
+            setMistake(false)
             if (counter == 0) {
                 setCatImg(catConfig.left)
             }
@@ -31,6 +33,7 @@ export default function Type() {
             if (event.code === 'Enter' && (event.ctrlKey || event.metaKey)) {
                 restart()
             }else {
+                setMistake(true)
                 setCatImg(catConfig.default)
             }
         }
@@ -59,17 +62,26 @@ export default function Type() {
             className=""
             style={{backgroundColor: theme.background}}
         >
-            <div className="container flex items-center justify-center min-h-[calc(100vh-6rem)]">
-                <div className="flex flex-col justify-center items-center w-[100%]">
+            <div className="container flex flex-col items-center justify-start min-h-[calc(100vh-6rem)]">
+                <div className="timer-wrapper flex justify-center items-center">
+                    <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <circle cx="20" cy="21" r="14" fill={theme.text}/>
+                        <path d="M20 13V20.382C20 20.7607 20.214 21.107 20.5528 21.2764L26 24" stroke={theme.background} strokeWidth="2" strokeLinecap="round"/>
+                        <path d="M7 9L13 5M7 9L13 5" stroke={theme.text} strokeWidth="4" strokeLinecap="round"/>
+                        <path d="M33 9L27 5M33 9L27 5" stroke={theme.text} strokeWidth="4" strokeLinecap="round"/>
+                    </svg>
+                    <span className="timer ml-2 font-normal text-[1.25rem]" style={{color: theme.text}}>0:18</span>
+                </div>
+
+                <div className="mt-[25vh] mb-[10vh] flex flex-col justify-center items-center w-[100%]">
                     <img 
                         className="block w-12"
                         src={catImg} 
                     />
 
-
                     <div className="text-line-wrapper relative w-full h-fit">
-                        <div className="text-line-start w-10 h-full absolute top-0 left-0 z-20 rounded-[100%]" style={{background: `linear-gradient(90deg, ${theme.header} 50%,  rgba(0,0,0,0) 100%)`}}></div>
-                        <div className="text-line-end w-10 h-full absolute top-0 right-0 z-20 rounded-[100%]" style={{background: `linear-gradient(90deg, rgba(0,0,0,0) 0%, ${theme.header} 50%)`}}></div>
+                        <div className="text-line-start lg:w-10 w-20 h-full absolute top-0 left-0 z-20 rounded-[100%]" style={{background: `linear-gradient(90deg, ${theme.header} 50%,  rgba(0,0,0,0) 100%)`}}></div>
+                        <div className="text-line-end lg:w-10 w-20 h-full absolute top-0 right-0 z-20 rounded-[100%]" style={{background: `linear-gradient(90deg, rgba(0,0,0,0) 0%, ${theme.header} 50%)`}}></div>
                         
                         <div
                             className="text-line relative w-full h-16 lg:h-12 overflow-hidden rounded-full z-5"
@@ -85,7 +97,13 @@ export default function Type() {
                                     (<span 
                                         key={index} 
                                         style={{opacity: index >= counter ? 0.8 : 1, position: 'relative', color: theme.text, overflow: 'visible'}}
-                                        className={index == counter ? `current-symbol text-[2rem] lg:text-[1.5rem]` : "text-[2rem] lg:text-[1.5rem]"}
+                                        className={
+                                            index == counter && mistake
+                                            ? `current-symbol text-mistake text-[2rem] lg:text-[1.5rem]`
+                                            : index == counter && !mistake
+                                                ? `current-symbol text-[2rem] lg:text-[1.5rem]`
+                                                : "text-[2rem] lg:text-[1.5rem]"
+                                        }
                                     >
                                         <div className="symbol-cursor" style={{backgroundColor: theme.accent}}></div>
                                         {symbol}
@@ -96,16 +114,17 @@ export default function Type() {
                     </div>
                     
 
-                    <div 
-                        className="opacity-40 hover:opacity-100 hover:cursor-pointer transition-opacity duration-200 ease-in-out mt-30 text-center flex flex-col justify-center items-center hover:[&>p]:opacity-0 hover:[&>p]:translate-y-1"
-                        onClick={restart}
-                    >
-                        <svg width="54" height="54" viewBox="0 0 54 54" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M36.364 20.636C31.6777 15.9497 24.0797 15.9497 19.3934 20.636C14.7071 25.3223 14.7071 32.9203 19.3934 37.6066C24.0797 42.2929 31.6777 42.2929 36.364 37.6066" stroke={theme.text} stroke-width="4" stroke-linecap="round"/>
-                            <path d="M37.412 15.6787L30.9082 22.2251C30.2755 22.8621 30.7375 23.9455 31.6352 23.9298L38.2517 23.8138C38.8039 23.8041 39.2437 23.3486 39.234 22.7964L39.1213 16.366C39.1058 15.4821 38.0351 15.0516 37.412 15.6787Z" fill={theme.text}/>
-                        </svg>
-                        <p className="text-[0.8rem] font-bold translate-x-0.5 transition-all duration-200 ease-in-out" style={{color: theme.text}}>Ctrl + Enter</p>
-                    </div>
+                </div>
+
+                <div 
+                    className="restart opacity-10 hover:opacity-100 hover:cursor-pointer transition-opacity duration-200 ease-in-out text-center flex flex-col justify-center items-center hover:[&>p]:opacity-0 hover:[&>p]:translate-y-1"
+                    onClick={restart}
+                >
+                    <svg width="54" height="54" viewBox="0 0 54 54" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M36.364 20.636C31.6777 15.9497 24.0797 15.9497 19.3934 20.636C14.7071 25.3223 14.7071 32.9203 19.3934 37.6066C24.0797 42.2929 31.6777 42.2929 36.364 37.6066" stroke={theme.text} strokeWidth="4" strokeLinecap="round"/>
+                        <path d="M37.412 15.6787L30.9082 22.2251C30.2755 22.8621 30.7375 23.9455 31.6352 23.9298L38.2517 23.8138C38.8039 23.8041 39.2437 23.3486 39.234 22.7964L39.1213 16.366C39.1058 15.4821 38.0351 15.0516 37.412 15.6787Z" fill={theme.text}/>
+                    </svg>
+                    <p className="text-[0.8rem] font-bold translate-x-0.5 transition-all duration-200 ease-in-out" style={{color: theme.text}}>Ctrl + Enter</p>
                 </div>
             </div>
         </main>
