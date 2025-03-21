6ba20b6c19cf18dcebf1a4147382eadd7bd4f247
ShebalkovD <shebalkovdenis.wk@gmail.com>
2025-03-21 13:15:32 +0300

    Добавлены доп. цветовые темы

--- src/assets/theme.js
+++ src/assets/theme.js
@@ -16,6 +16,42 @@ let themes = {
         text: "#DEE4F5",
         accent: "#019AFF",
         textline: "#1E222D"
+    },
+    matrix: {
+        id: 3,
+        name: "matrix",
+        header: "#131613",
+        background: "#1A1B1E",
+        text: "#DEE4F5",
+        accent: "#04EA00",
+        textline: "#1131613"
+    },
+    dark_funeral: {
+        id: 4,
+        name: "dark_funeral",
+        header: "#000",
+        background: "#121212",
+        text: "#aaaaab",
+        accent: "#5f8787",
+        textline: "#222222"
+    },
+    dinosaur_tail: {
+        id: 5,
+        name: "dinosaur_tail",
+        header: "#0B453A",
+        background: "#03624C",
+        text: "#F1F7F6",
+        accent: "#032221",
+        textline: "#222222"
+    },
+    pinky: {
+        id: 6,
+        name: "pinky",
+        header: "#292451",
+        background: "#3B3473",
+        text: "#efe3ff",
+        accent: "#ea6890",
+        textline: "#222222"
     }
 }
 
